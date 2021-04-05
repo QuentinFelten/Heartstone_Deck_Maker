e65c592a7fbe21c8e1b0515d30220d4fb8bedcc4
Celipot <61536494+Celipot@users.noreply.github.com>
2021-04-05 17:15:53 +0200

    fixed branch

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { cardHs } from '../../interface/card-hs.interface';
+import { ApiService } from './../api.service';
 
 @Component({
-  selector: 'app-card-list',
+  selector: 'app-root',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent implements OnInit {
+	public images:Array<cardHs> = new Array<cardHs>();
 
-  constructor() { }
+	constructor(private hearthstone:ApiService) {
+	}
+	ngOnInit(): void {
+		
+		this.hearthstone.getInfo().then((data)=> {this.images = data.filter(this.isNotHero)
+			console.log(this.images)})
+		console.log(this.images);
+		
 
-  ngOnInit(): void {
-  }
+	}
 
+	isNotHero(card:cardHs){
+		return card.type != "Hero"
+	  }
+	
+  
 }
